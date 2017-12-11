84729f0b49293b241c9aa3b37ab048cb6429aa20
tintinmovie <31559358+tintinmovie@users.noreply.github.com>
2017-12-12 09:08:43 +1300

    Rename Tic-Tac-Toe/strings-spanish.cpp to Tic-Tac-Toe/src/ES/strings-spanish_ES.cpp

--- Tic-Tac-Toe/src/ES/strings-spanish_ES.cpp
+++ Tic-Tac-Toe/src/ES/strings-spanish_ES.cpp
@@ -12,7 +12,7 @@ void banner_ttt()
 	cout<<"-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
 	cout<<"\n";
 	cout<<"Este es un juego de 2 jugadores en el cual '0' va antes de 'X' ^_^ \n";
-	cout<<"El/la jugador/a debe introducir la posición en la cual quiere que     \n";
+	cout<<"El/la jugador/a debe introducir la posiciÃ³n en la cual quiere que     \n";
 	cout<<"se coloque el '0' o la 'X'\n\n\n";
 
 		// Codigo para el banner va aca
